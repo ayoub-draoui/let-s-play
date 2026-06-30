bd0573ec346e982e528c8dcb6933ce2cae004cfa
ayoub-draoui <mr.stefan.draoui@gmail.com>
2026-06-30 01:28:22 +0100

    starting the project

---
+++ src/test/java/let/s_play/dev/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package let.s_play.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
